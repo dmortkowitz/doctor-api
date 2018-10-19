e700eebb07e5fb679fd56567ef0cbecc911499f0
David Mortkowitz <dmortkowitz@gmail.com>
2018-10-19 12:22:31 -0700

    Add step-one of background js.

--- src/doctor.js
+++ src/doctor.js
@@ -1,1 +1,28 @@
 import { apiKey } from './../.env';
+
+export class Doctor {
+  constructor() {
+    this.state = 'wa-seattle';
+    this.dataSize;
+  }
+
+  getDoctorAilment(search) {
+    let searchData = $.ajax ({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=${this.state}&skip=0&limit=25&user_key=${apiKey}`,
+      type: 'GET',
+      async: false,
+      searchData: {
+        format: 'json'
+      },
+      success: function(response) {
+        let data = response.data;
+      },
+      error: function() {
+        $("#errorField").text("There was an error retrieving your information. PLease try again.");
+      }
+    });
+    return searchData;
+  }
+
+
+}
