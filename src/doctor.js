76e2e87f43a27b236d0442c42e468f56e143734e
David Mortkowitz <dmortkowitz@gmail.com>
2018-10-19 12:27:56 -0700

    Add second part of back-end code, with successful compile.

--- src/doctor.js
+++ src/doctor.js
@@ -15,6 +15,7 @@ export class Doctor {
         format: 'json'
       },
       success: function(response) {
+        console.log(response);
         let data = response.data;
       },
       error: function() {
@@ -23,6 +24,23 @@ export class Doctor {
     });
     return searchData;
   }
-
-
+  getDoctorName(docName) {
+    let data = $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=${this.state}&skip=0&limit=20&user_key=${apiKey}`,
+      type: 'GET',
+      async: false,
+      data: {
+        format: 'json'
+      },
+      success: function(response, jqXHR, status) {
+      console.log(response);
+      console.log(jqXHR);
+      console.log(status);
+      },
+      error: function() {
+        $('#errorField').text("There was an error retrieving information. Please try again.");
+      }
+    });
+    return data;
+  }
 }
